e87004c500a8f5d920ccc86e1a25d1a1d279e85c
BuildTools <unconfigured@null.spigotmc.org>
2024-02-29 17:12:58 -0500

    Added Nightmare

---
+++ src/main/java/net/swimmingtuna/lotm/item/custom/BeyonderAbilities/Nightmare.java
@@ -0,0 +1,113 @@
+package net.swimmingtuna.lotm.item.custom.BeyonderAbilities;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+import net.minecraftforge.common.ForgeMod;
+import net.minecraftforge.common.util.LazyOptional;
+import net.swimmingtuna.lotm.caps.BeyonderHolderAttacher;
+import net.swimmingtuna.lotm.events.ReachChangeUUIDs;
+import net.swimmingtuna.lotm.spirituality.ModAttributes;
+import net.swimmingtuna.lotm.util.effect.ModEffects;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class Nightmare extends Item implements ReachChangeUUIDs {
+    private final LazyOptional<Multimap<Attribute, AttributeModifier>> lazyAttributeMap = LazyOptional.of(() -> createAttributeMap()); //LazyOptional in this instance basically makes it so that the reach change is only in effect when something happens
+
+    public Nightmare(Properties pProperties) {
+        super(pProperties);
+    }
+    @Override
+    public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot pSlot) {
+        if (pSlot == EquipmentSlot.MAINHAND) {
+            return lazyAttributeMap.orElseGet(() -> createAttributeMap());
+        }
+        return super.getDefaultAttributeModifiers(pSlot);
+    }
+
+    private Multimap<Attribute, AttributeModifier> createAttributeMap() {
+
+        ImmutableMultimap.Builder<Attribute, AttributeModifier> attributeBuilder = ImmutableMultimap.builder();
+        attributeBuilder.putAll(super.getDefaultAttributeModifiers(EquipmentSlot.MAINHAND));
+        attributeBuilder.put(ForgeMod.ENTITY_REACH.get(), new AttributeModifier(BeyonderEntityReach, "Reach modifier", 35, AttributeModifier.Operation.ADDITION)); //adds a 12 block reach for interacting with entities
+        attributeBuilder.put(ForgeMod.BLOCK_REACH.get(), new AttributeModifier(BeyonderBlockReach, "Reach modifier", 35, AttributeModifier.Operation.ADDITION)); //adds a 12 block reach for interacting with blocks, p much useless for this item
+        return attributeBuilder.build();
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        Player pPlayer = pContext.getPlayer();
+        Level level = pPlayer.level();
+        BlockPos positionClicked = pContext.getClickedPos();
+        if (!pContext.getLevel().isClientSide) {
+            BeyonderHolderAttacher.getHolder(pPlayer).ifPresent(spectatorSequence -> {
+                if (spectatorSequence.getCurrentSequence() <= 5 &&  BeyonderHolderAttacher.getHolderUnwrap(pPlayer).useSpirituality(100)) {
+                    useNightmare(pPlayer, level, positionClicked, spectatorSequence.getCurrentSequence());
+                    if (!pPlayer.getAbilities().instabuild) {
+                        pPlayer.getCooldowns().addCooldown(this, 110);
+                    }
+                }
+            });
+        }
+        return InteractionResult.SUCCESS;
+    }
+
+    private void useNightmare(Player pPlayer, Level level, BlockPos targetPos, int sequence) {
+            double radius = 25.0 - sequence;
+            float damagePlayer = (float) 120.0 - (sequence * 10);
+            float damageMob = (float) (50.0 - (sequence * 3)) / 2;
+
+        int duration = 200 - (sequence * 20);
+
+        AABB boundingBox = new AABB(targetPos).inflate(radius);
+        level.getEntitiesOfClass(LivingEntity.class, boundingBox, entity -> entity.isAlive()).forEach(livingEntity -> {
+            AttributeInstance nightmareAttribute = livingEntity.getAttribute(ModAttributes.NIGHTMARE.get());
+            String playerName = livingEntity.getDisplayName().getString();
+            //if (livingEntity != pPlayer) {
+                livingEntity.addEffect(new MobEffectInstance(MobEffects.DARKNESS, duration, 1, false, false));
+                if (livingEntity instanceof Player) {
+                    if (nightmareAttribute.getValue() < 3) {
+                        nightmareAttribute.setBaseValue(nightmareAttribute.getValue() + 1);}
+                if (nightmareAttribute.getValue() == 3) {
+                    livingEntity.hurt(livingEntity.damageSources().magic(), damagePlayer);
+                    nightmareAttribute.setBaseValue(0);
+                }
+                    pPlayer.sendSystemMessage(Component.literal(playerName + "'s nightmare value is:" + nightmareAttribute.getValue()));
+
+                }
+                else {
+                    livingEntity.hurt(livingEntity.damageSources().magic(), damageMob);
+                }
+            //}
+        });
+    }
+    @Override
+    public void appendHoverText(@NotNull ItemStack pStack, @Nullable Level level, List<Component> componentList, TooltipFlag tooltipFlag) {
+        if (!Screen.hasShiftDown()) {
+            componentList.add(Component.literal("Upon use, makes all players around the target enter a nightmare, plunging them into darkness. If this is used 3 times on a player within 30 seconds, they take immense damage. If it's used on a mob, they take less the damage without having to be hit multiple times.\n" +
+                    "Spirituality Used: 100\n" +
+                    "Cooldown: 5.5 seconds"));
+        }
+        super.appendHoverText(pStack, level, componentList, tooltipFlag);
+    }
+}
---
+++ src/main/java/net/swimmingtuna/lotm/util/effect/NightmareEffect.java
@@ -0,0 +1,77 @@
+package net.swimmingtuna.lotm.util.effect;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+
+import java.util.Random;
+
+public class NightmareEffect extends MobEffect {
+    private final Random random = new Random();
+    private int nightmareApplyCounter = 0;
+
+    public NightmareEffect(MobEffectCategory mobEffectCategory, int color) {
+        super(mobEffectCategory, color);
+    }
+
+    @Override
+    public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
+        if (!pLivingEntity.level().isClientSide()) {
+            nightmareApplyCounter++;
+            if (nightmareApplyCounter >= 50) {
+                double x = pLivingEntity.getX();
+                double y = pLivingEntity.getY();
+                double z = pLivingEntity.getZ();
+
+
+                SoundEvent randomSound = MOB_SOUNDS[random.nextInt(MOB_SOUNDS.length)];
+                Player pPlayer = (Player) pLivingEntity;
+                pLivingEntity.level().playSound(null, pLivingEntity.getX(), pLivingEntity.getY(), pLivingEntity.getZ(), randomSound, SoundSource.HOSTILE.AMBIENT, 1.0F, 1.0F);
+                pLivingEntity.level().playLocalSound(x,y,z, randomSound,  SoundSource.HOSTILE.AMBIENT, 1.0F, 1.0F, false);
+                nightmareApplyCounter = 0;
+            }
+        }
+        super.applyEffectTick(pLivingEntity, pAmplifier);
+    }
+
+    @Override
+    public boolean isDurationEffectTick(int pDuration, int pAmplifier) {
+        return true;
+    }
+
+    private static final SoundEvent[] MOB_SOUNDS = {
+            SoundEvents.CREEPER_PRIMED,
+            SoundEvents.SPIDER_STEP,
+            SoundEvents.GENERIC_EXPLODE,
+            SoundEvents.ELDER_GUARDIAN_CURSE,
+            SoundEvents.ENDERMAN_STARE,
+            SoundEvents.ZOMBIE_DEATH,
+            SoundEvents.GHAST_SCREAM,
+            SoundEvents.EVOKER_FANGS_ATTACK,
+            SoundEvents.EVOKER_PREPARE_ATTACK,
+            SoundEvents.STRAY_AMBIENT,
+            SoundEvents.ZOMBIE_VILLAGER_CONVERTED,
+            SoundEvents.SPIDER_AMBIENT,
+            SoundEvents.SPIDER_HURT,
+            SoundEvents.CAT_PURR,
+            SoundEvents.ENDER_DRAGON_GROWL,
+            SoundEvents.ENDERMAN_AMBIENT,
+            SoundEvents.SKELETON_HORSE_AMBIENT,
+            SoundEvents.SILVERFISH_STEP,
+            SoundEvents.SKELETON_STEP,
+            SoundEvents.ZOMBIE_ATTACK_WOODEN_DOOR,
+            SoundEvents.VEX_CHARGE,
+            SoundEvents.ZOGLIN_ANGRY,
+            SoundEvents.ZOGLIN_STEP,
+            SoundEvents.GUARDIAN_AMBIENT,
+            SoundEvents.ENDERMITE_STEP,
+            SoundEvents.ELDER_GUARDIAN_AMBIENT,
+            SoundEvents.PHANTOM_DEATH,
+    };
+}
