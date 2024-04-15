81d6f349267e940acfd82a8d7967e6ab5591eeaf
BuildTools <unconfigured@null.spigotmc.org>
2024-04-15 16:19:48 -0400

    v confused on entity damage

--- src/main/java/net/swimmingtuna/lotm/entity/AqueousLightEntity.java
+++ src/main/java/net/swimmingtuna/lotm/entity/AqueousLightEntity.java
@@ -1,12 +1,9 @@
 package net.swimmingtuna.lotm.entity;
 
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.Component;
+
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -44,13 +41,13 @@ public class AqueousLightEntity extends AbstractHurtingProjectile {
     }
 
 
-    @Override
     protected void onHitEntity(EntityHitResult pResult) {
         if (pResult.getEntity() instanceof LivingEntity entity) {
-            entity.hurt(entity.damageSources().magic(),5);
+            entity.hurt(damageSources().fall(), 5);
         }
     }
 
+    @Override
     protected void onHit(HitResult pResult) {
         if (!this.level().isClientSide) {
             this.level().broadcastEntityEvent(this, ((byte) 3));
@@ -63,10 +60,6 @@ public class AqueousLightEntity extends AbstractHurtingProjectile {
         return false;
     }
 
-
-    public boolean hurt(DamageSource pSource, float pAmount) {
-        return false;
-    }
 
     protected void defineSynchedData() {
         this.entityData.define(DATA_DANGEROUS, false);
