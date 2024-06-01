6c64a1058dc03a496534e32a43e16eefe791be0b
BuildTools <unconfigured@null.spigotmc.org>
2024-06-01 13:05:30 -0400

    p much done w/ spectator other than envision kingdom shite

---
+++ src/main/java/net/swimmingtuna/lotm/events/ClientEvents2.java
@@ -0,0 +1,20 @@
+package net.swimmingtuna.lotm.events;
+
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.swimmingtuna.lotm.LOTM;
+import net.swimmingtuna.lotm.init.ParticleInit;
+import net.swimmingtuna.lotm.particle.AcidRainParticle;
+import net.swimmingtuna.lotm.particle.NullParticle;
+
+@Mod.EventBusSubscriber(modid = LOTM.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientEvents2 {
+    @SubscribeEvent
+    public static void registerParticleProvidersEvent(final RegisterParticleProvidersEvent event) {
+        event.registerSpriteSet(ParticleInit.ACIDRAIN_PARTICLE.get(), AcidRainParticle.Provider::new);
+        event.registerSpriteSet(ParticleInit.NULL_PARTICLE.get(), NullParticle.Provider::new);
+    }
+}
